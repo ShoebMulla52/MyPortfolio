86f33d8cf974dcba4cb0afe5c67200d6a1b942f3
Shoeb Mulla <mullashoeb83@gmail.com>
2026-01-11 09:40:29 +0530

    added animations

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function showMessage() {
     alert("Thank you for your interest! Please contact me via email.");
 }
+
+
+
+// Scroll animation
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+    const triggerBottom = window.innerHeight / 5 * 4;
+
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+
+        if (sectionTop < triggerBottom) {
+            section.classList.add("show");
+        }
+    });
+});
